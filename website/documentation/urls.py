5806633d328e60da3d21bf0a5b2c68cedd823fd9
rowinggolfer-cs50 <rowinggolfer+cs50@gmail.com>
2020-12-30 16:58:18 +0000

    ready to present

--- website/documentation/urls.py
+++ website/documentation/urls.py
@@ -4,4 +4,39 @@ from documentation import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="documentation-homepage"),
+    path(
+        "create-venv",
+        views.VenvView.as_view(),
+        name="documentation-create-venv",
+    ),
+    path(
+        "install-django",
+        views.DjangoInstallView.as_view(),
+        name="documentation-install-django",
+    ),
+    path(
+        "start-django",
+        views.DjangoStartView.as_view(),
+        name="documentation-start-django",
+    ),
+    path(
+        "sqlite",
+        views.Sqlite3View.as_view(),
+        name="documentation-sqlite",
+    ),
+    path(
+        "runserver",
+        views.RunserverView.as_view(),
+        name="documentation-runserver",
+    ),
+    path(
+        "security",
+        views.SecurityView.as_view(),
+        name="documentation-security",
+    ),
+    path(
+        "index-app",
+        views.IndexAppView.as_view(),
+        name="documentation-indexapp",
+    ),
 ]
--- website/documentation/views.py
+++ website/documentation/views.py
@@ -3,3 +3,31 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "documentation/index.html"
+
+
+class VenvView(TemplateView):
+    template_name = "documentation/venv.html"
+
+
+class DjangoInstallView(TemplateView):
+    template_name = "documentation/install_django.html"
+
+
+class DjangoStartView(TemplateView):
+    template_name = "documentation/create_website.html"
+
+
+class Sqlite3View(TemplateView):
+    template_name = "documentation/django_database.html"
+
+
+class RunserverView(TemplateView):
+    template_name = "documentation/test_server.html"
+
+
+class SecurityView(TemplateView):
+    template_name = "documentation/django_security.html"
+
+
+class IndexAppView(TemplateView):
+    template_name = "documentation/django_apps.html"
